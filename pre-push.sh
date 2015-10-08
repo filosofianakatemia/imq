a168eeb9f7e2584328301541c49963fbc50bfb82
JP Salo <jukapeka@gmail.com>
2015-10-08 09:58:17 +0300

    Fixed a typo.

--- pre-push.sh
+++ pre-push.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -a --delete --exclude 'pre-push.sh' --exclude '*.sublime-project' --exclude '*.sublime-workspace' -delete-exluded ~/devel/imq/ ~/Dropbox/Filosofian\ Akatemia\ -\ IMQ/skriptit
+rsync -a --delete --exclude 'pre-push.sh' --exclude '*.sublime-project' --exclude '*.sublime-workspace' --delete-excluded ~/devel/imq/ ~/Dropbox/Filosofian\ Akatemia\ -\ IMQ/skriptit
