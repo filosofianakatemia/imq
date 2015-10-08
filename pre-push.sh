82194de3f5ad31b58c07d17a7563bfa52aa7133b
JP Salo <jukapeka@gmail.com>
2015-10-08 09:50:06 +0300

    Exlude Sublime workspace and project files from pre-push git hook to sync scripts to Dropbox.

--- pre-push.sh
+++ pre-push.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -a --delete --exclude 'pre-push.sh' ~/devel/imq/ ~/Dropbox/Filosofian\ Akatemia\ -\ IMQ/skriptit
+rsync -a --delete --exclude 'pre-push.sh' --exclude '*.sublime-project' --exclude '*.sublime-workspace' ~/devel/imq/ ~/Dropbox/Filosofian\ Akatemia\ -\ IMQ/skriptit
