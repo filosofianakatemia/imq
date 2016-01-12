9f847e41be739617a9d0d0407f249d19b39e596f
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2016-01-12 21:00:52 +0200

    Removed sublime project which did not apparently work

--- pre-push.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rsync -a --delete --exclude 'pre-push.sh' --exclude '*.sublime-project' --exclude '*.sublime-workspace' --delete-excluded ~/devel/imq/ ~/Dropbox/Filosofian\ Akatemia\ -\ IMQ/skriptit
